4d8bf17738f274297897087810441da291209ce6
JustinChien <justin6302971@hotmail.com>
2024-08-16 21:47:21 +0800

    Update crawler_url.py

--- constants/crawler_url.py
+++ constants/crawler_url.py
@@ -1,2 +1,2 @@
 ZHUBEI_GOLF_STORE_URL= "https://golf24.com.tw/vrgolf/room.html?130112032710133920"
-OTHER_GOLF_STORE_URL= "https://golf24.com.tw/vrgolf/room.html?130118051512109843"
+ZHUBEI_HSR_GOLF_STORE_URL= "https://golf24.com.tw/vrgolf/room.html?130128070312263491"
