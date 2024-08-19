be0bc27e7412626eb1f1dc4425a6c1694933bf01
justin6302971 <justin6302971@hotmail.com>
2024-08-19 23:42:15 +0800

    dockerize app and setup docker compose file for dependencies

--- app.py
+++ app.py
@@ -6,39 +6,65 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+
+URL = "http://selenium-chrome:4444/wd/hub"
 
 
 def check_latest_golfstore_info():
+
     now = datetime.now()
     now_at_hour_level = now.replace(minute=0, second=0, microsecond=0)
 
     today6am = now.replace(hour=6, minute=0, second=0, microsecond=0)
-    
+
     tomorrow0am = (now + timedelta(days=1)).replace(
         hour=0, minute=0, second=0, microsecond=0
     )
-    try: 
+    try:
         if today6am <= now_at_hour_level and now_at_hour_level <= tomorrow0am:
             formatted_now = now.strftime("%Y-%m-%d %H:%M:%S")
             print(f"start at: {formatted_now}--------")
-            browser = webdriver.Chrome()
 
-            golf_store_urls = [ZHUBEI_GOLF_STORE_URL, ZHUBEI_HSR_GOLF_STORE_URL]
+            # browser = webdriver.Chrome()
+
+            golf_store_urls = [ZHUBEI_GOLF_STORE_URL,
+                               ZHUBEI_HSR_GOLF_STORE_URL]
 
             for golf_store_url in golf_store_urls:
                 print(f"\n--------")
+                time.sleep(15)
+
+                options = webdriver.ChromeOptions()
+                options.add_argument('--ignore-ssl-errors=yes')
+                options.add_argument('--ignore-certificate-errors')
+                options.add_argument('--disable-dev-shm-usage')
+                options.add_argument('--no-sandbox')
+                options.add_argument('--headless')
+                options.add_argument("--disable-gpu")
+                options.add_argument("--disable-extensions")
+                options.add_argument("--window-size=1920x1080")
+
+                browser = webdriver.Remote(
+                    command_executor=URL,
+                    options=options
+                )
+               
+                browser.implicitly_wait(5)  # seconds
+
                 browser.get(golf_store_url)
                 wait = WebDriverWait(browser, timeout=20)
                 revealed = browser.find_element(By.ID, "app")
                 wait.until(lambda d: revealed.is_displayed())
+
                 get_golf_data(browser)
                 print(f"--------\n")
 
             browser.quit()
             print(f"end--------\n")
     except Exception as e:
+        print(e)
         pass
-        
 
 
 def get_golf_data(browser):
